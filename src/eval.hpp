e4a73cec254b14b1107fa0dee7bcba70a383e935
ToruNiina <niina.toru.68u@gmail.com>
2019-05-03 19:22:50 +0900

    feat: enable to evaluate func_t

--- src/eval.hpp
+++ src/eval.hpp
@@ -23,6 +23,22 @@ inline std::vector<object_t> make_list(const cell_t& cell)
 
 object_t eval(const object_t& obj, env_t& env);
 
+object_t apply(const func_t& fn, const object_t& args, env_t& env)
+{
+    const auto arguments = make_list(std::get<cell_t>(args.data));
+    if(arguments.size() != fn.args.size())
+    {
+        throw std::runtime_error("[error] lacking function arguments");
+    }
+
+    for(std::size_t i=0; i<fn.args.size(); ++i)
+    {
+        env[fn.args.at(i)] = eval(arguments.at(i), env);
+    }
+
+    return eval(fn.body, env);
+}
+
 struct evaluator
 {
     evaluator(env_t& e): env(std::ref(e)) {}
@@ -36,7 +52,8 @@ struct evaluator
     object_t operator()(const nil_t&)          {return object_t(nil);}
     object_t operator()(const bool& v)         {return object_t(v);}
     object_t operator()(const std::int64_t& v) {return object_t(v);}
-    object_t operator()(const builtin_t& v)       {return object_t(v);}
+    object_t operator()(const builtin_t& v)    {return object_t(v);}
+    object_t operator()(const func_t& v)       {return object_t(v);}
     object_t operator()(const symbol_t& symbol)
     {
         if(env.get().objs.count(symbol) == 0)
@@ -48,12 +65,16 @@ struct evaluator
     object_t operator()(const cell_t& c)
     {
         auto front = eval(car(c), env.get()); // if it is a symbol, search that
-        if(not std::holds_alternative<builtin_t>(front.data))
+        if(std::holds_alternative<builtin_t>(front.data))
         {
-            throw std::runtime_error(
-                "[error] first value of list must be a func to be evaluated");
+            return std::get<builtin_t>(front.data).fn(cdr(c), env);
         }
-        return std::get<builtin_t>(front.data).fn(cdr(c), env);
+        else if(std::holds_alternative<func_t>(front.data))
+        {
+            return apply(std::get<func_t>(front.data), cdr(c), env);
+        }
+        throw std::runtime_error(
+            "[error] first value of list must be a func to be evaluated");
     }
 
     std::reference_wrapper<env_t> env;
